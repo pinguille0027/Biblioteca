690dabe4d84ffa1323520facab7826b43d4a8371
pinguille <guillermo.orio27@gmail.com>
2022-12-29 15:57:39 +0100

    primeros pasos de la api y mejorando diversos asuntos

--- script biblioteca.sql
+++ script biblioteca.sql
@@ -5,46 +5,50 @@
     
 /*CREAR TABLAS Y PROHIBIMOS VALORES NULOS*/
 	CREATE TABLE Catalogo(
-	Sinatura INT (4) PRIMARY KEY,
-	Título VARCHAR (50) NOT NULL,
-    Autor VARCHAR (30),
-	Editorial VARCHAR (30) NOT NULL,
-    Año_de_Publicación INT(4),
-	Disponibilidad BIT 
+        Sinatura INT NOT NULL AUTO_INCREMENT,
+        Titulo VARCHAR (50) NOT NULL,
+        Autor VARCHAR (30),
+        Editorial VARCHAR (30) NOT NULL,
+        Anno_de_Publicacion INT(4),
+        Disponibilidad BIT,
+        PRIMARY KEY(Sinatura)
     );
 	
 	CREATE TABLE Usuario(
-	Id_Usuario INT(5) PRIMARY KEY ,
-	nombre VARCHAR (30) NOT NULL  ,
-    apellidos VARCHAR (30) NOT NULL  ,
-    DNI CHAR(9) UNIQUE NOT NULL ,
-    Telefono INT (9) NOT NULL,
-    Fecha_de_alta DATE NOT NULL,
-    Clave_de_acceso  VARCHAR (30) NOT NULL
+        Id INT NOT NULL AUTO_INCREMENT,
+        nombre VARCHAR (30) NOT NULL  ,
+        apellidos VARCHAR (30) NOT NULL  ,
+        DNI CHAR(9) UNIQUE NOT NULL ,
+        Telefono INT (9) NOT NULL,
+        Fecha_de_alta DATE NOT NULL,
+        Clave_de_acceso  VARCHAR (30) NOT NULL,
+        PRIMARY KEY(Id)
 	);
     
     CREATE TABLE Pedidos(
-	Id_Pedido INT PRIMARY KEY ,
-	Id_Usuario INT (5) NOT NULL,
-    Nombre_Usuario VARCHAR (30) NOT NULL,
-    Libro_prestado INT   NOT NULL,
-    Titulo_Libro VARCHAR (30) NOT NULL,
-    Fecha_Pedido DATETIME   NOT NULL,
-    Fecha_Devolución DATETIME NOT NULL 
+        Id INT  NOT NULL AUTO_INCREMENT,
+        Id_Usuario INT (5) NOT NULL,
+        Nombre_Usuario VARCHAR (30) NOT NULL,
+        Libro_prestado INT   NOT NULL,
+        Titulo_Libro VARCHAR (30) NOT NULL,
+        Fecha_Pedido DATETIME   NOT NULL,
+        Fecha_Devolucion DATETIME NOT NULL,
+        PRIMARY KEY(Id)
 	);
 
 /*Foraneas*/
-    ALTER TABLE Pedidos ADD CONSTRAINT FK_idus FOREIGN KEY (Id_Usuario) REFERENCES Usuario(Id_Usuario);																			
+    ALTER TABLE Pedidos ADD CONSTRAINT FK_idus FOREIGN KEY (Id_Usuario) REFERENCES Usuario(Id);																			
 	ALTER TABLE Pedidos ADD CONSTRAINT FK_idlib FOREIGN KEY (Libro_prestado) REFERENCES Catalogo(Sinatura);
 
 /*Valores*/
-    INSERT INTO Catalogo 	VALUES	( 0001, 'A Esmorga',    'Eduardo Blanco Amor', 'Galaxia', 1960, 1),
-                                    ( 0002, 'Crónica de una muerte anunciada',    'Gabriel García Marquez', 'santillana', 1965, 0),
-                                    ( 0003, 'Cantares Galegos',    'Rosalía de Castro', 'Galaxia', 1865, 0),
-                                    ( 0004, 'El Hobbit',    'Tolkien', 'Oxford', 1930, 1),
-                                    ( 0005, 'O porco de pé',    'Vicente Risco', 'Galaxia', 1955, 1),
-                                    ( 0006, 'Lazarillo de Tormes',    NULL, 'santillana', 1570, 1),
-                                    ( 0007, 'La Biblia',    NULL, 'Conferencia Episcopal', NULL, 1);
+    INSERT INTO Catalogo (Titulo, Autor, Aditorial, Anno_de_Publicacion, Disponibilidad) 
+    VALUES	( 'A Esmorga',    'Eduardo Blanco Amor', 'Galaxia', 1960, 1),
+            ( 'Crónica de una muerte anunciada',    'Gabriel García Marquez', 'santillana', 1965, 0),
+            ( 'Cantares Galegos',    'Rosalía de Castro', 'Galaxia', 1865, 0),
+            ( 'El Hobbit',    'Tolkien', 'Oxford', 1930, 1),
+            ( 'O porco de pé',    'Vicente Risco', 'Galaxia', 1955, 1),
+            ( 'Lazarillo de Tormes',    NULL, 'santillana', 1570, 1),
+            ( 'La Biblia',    NULL, 'Conferencia Episcopal', NULL, 1);
 
     INSERT INTO Usuario 	VALUES	( 00001, 'Ramona',    'Seixas Martinez', '36456097L', 681266780, '2019-05-19', 'uwu1'),
                                     ( 00002, 'Angustias',    'lopez abreu', '38954112F', 656009581, '2018-10-30', 'uwu2'),
---
+++ scripts.js
@@ -0,0 +1,4 @@
+const doc = document.getElementById('hyphen');
+
+doc.classList.remove('footer--hyphen__error');
+doc.classList.add('footer--hyphen__success');
