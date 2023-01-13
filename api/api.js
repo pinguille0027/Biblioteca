e9e9036059d44e370336b5ce365e28028e43750b
pinguille0027 <guillermo.orio27@gmail.com>
2023-01-13 13:22:56 +0100

    prisma round 2

--- api/api.js
+++ api/api.js
@@ -2,63 +2,39 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan')
 const app = express();
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 const port = process.env.PORT || 8080;
 app.use(morgan())
+app.use(express.json())
 
 
-const libros = [
-  {
-    sinatura: 1234,
-    titulo: 'La historia interminable',
-    autor: null,
-    editorial: 'Galaxia',
-    annoDePublicacion: 1900,
-    disponibilidad: true
-  },
-  {
-    sinatura: 1235,
-    titulo: 'Biblia',
-    autor: null,
-    editorial: 'None',
-    annoDePublicacion: 0,
-    disponibilidad: false
-  },
-  {
-    sinatura: 1235,
-    titulo: 'Biblia',
-    autor: null,
-    editorial: 'None',
-    annoDePublicacion: 0,
-    disponibilidad: false
-  },
-  {
-    sinatura: 1235,
-    titulo: 'Biblia',
-    autor: null,
-    editorial: 'None',
-    annoDePublicacion: 0,
-    disponibilidad: false
-  },
-  {
-    sinatura: 1235,
-    titulo: 'Biblia',
-    autor: null,
-    editorial: 'None',
-    annoDePublicacion: 0,
-    disponibilidad: false
-  }
-];
-
-app.get('/libros', function(req, res) {
-  res.status(200).json(libros);
+app.get('/', async(req, res) => {
+  try {
+  const libro = await prisma.libros.findMany();
+  res.json({libro});
+} catch (err) {
+  res.status(500).json({ error: err.message });
+}
 });
+
+
 
 // sendFile will go here
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../cli/index.html'));
 });
 
-app.use(express.static(path.join(__dirname, '../cli/')))
+app.use(express.static(path.join(__dirname, '../cli/')));
 
+/*app.get('/', async(req, res) => {
+  try {
+  const libro = await prisma.libros.findMany();
+  res.json({libro});
+} catch (err) {
+  res.status(500).json({ error: err.message });
+}
+});
+*/
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
